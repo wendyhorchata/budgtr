ed379465e4e63f0a5937ece328d47f18cff06ac3
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-04-23 20:33:00 -0700

    third commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
-const budget = require("./models/budget");
+const budgets = require("./models/budgets");
 const methodOverride = require(`method-override`);
 
 // Middleware
@@ -11,13 +11,8 @@ app.use(methodOverride(`_method`))
 
 
 // Index
-app.get("/budget/", (req, res) => {
+app.get("/budgets/", (req, res) => {
     res.render("index.ejs", { allBudgets: budgets, title: "Budgets - Index Page"});
-  });
-
-// Show
-app.get("/budget/:indexOfBudgetsArray", (req, res) => {
-    res.render("show.ejs", { budget: budgets[req.params.indexOfBudgetsArray], title: "First - Show Page" });
   });
 
 //New
@@ -25,10 +20,16 @@ app.get("/budget/new", (req, res) => {
     res.render("new.ejs", {title: "Budget - New Page"});
   });
 
+//Show
+  app.get("/budget/:indexOfBudgetsArray", (req, res) => {
+    res.render("show.ejs", { budget: budgets[req.params.indexOfBudgetsArray], title: "First - Show Page" });
+  });
+
 //Create
-app.post("/budget", (req, res) => {
-    budgets.push(req.body)
-  res.redirect("/budget")
+  app.post("/budget/", (req, res) => {
+  const id = budgets.length 
+  budgets.push(req.body)
+  res.redirect(`/budget/${id}`)
   });
 
 
